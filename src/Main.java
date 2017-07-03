765c5692e5c1c4c800aebb516bc437aaa919871c
IamHimon <539916384@qq.com>
2017-07-03 17:37:11 +0800

    wujiang coded

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //BufferedReader是可以按行读取文件
+        FileInputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+
+        String str;
+        try{
+            inputStream = new FileInputStream("C:\\Users\\student\\Desktop\\xiaoyu\\数据\\B1.txt");
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            while((str = bufferedReader.readLine()) != null)
+            {
+                System.out.println(str);
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("找不到指定文件");
+        }catch (IOException e){
+            System.out.println("读取文件失败");
+        } finally {
+        try {
+            assert bufferedReader != null;
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     }
 }
